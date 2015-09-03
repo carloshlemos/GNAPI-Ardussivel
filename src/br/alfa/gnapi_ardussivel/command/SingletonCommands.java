b43cdeb36aaa45acc4781f201089f368a95feccc
carloshlemos <chenriquelemos@gmail.com>
2015-09-03 10:58:44 -0300

    Singleton refatorado.

--- src/br/alfa/gnapi_ardussivel/command/SingletonCommands.java
+++ src/br/alfa/gnapi_ardussivel/command/SingletonCommands.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import br.alfa.gnapi_ardussivel.utensilio.ArCondicionado;
 import br.alfa.gnapi_ardussivel.utensilio.Lampada;
+import br.alfa.gnapi_ardussivel.utensilio.Portao;
 import br.alfa.gnapi_ardussivel.utensilio.Televisor;
 import br.alfa.gnapi_ardussivel.util.Command;
 
@@ -12,24 +13,25 @@ public class SingletonCommands {
 
 	private static SingletonCommands instance;
 
-	public static Map<String, Command> mapCommands = new HashMap<String, Command>();
+	public Map<String, Command> mapCommands;
 
 	public static SingletonCommands getInstance() {
 		if (instance == null) {
-			
 			instance = new SingletonCommands();
-			
-			mapCommands.put("lâmpada ligar", new UtensilioLigarCommand(new Lampada()));
-			mapCommands.put("lâmpada desligar", new UtensilioDesligarCommand(new Lampada()));
-			mapCommands.put("ar condicionado ligar", new UtensilioLigarCommand(new ArCondicionado()));
-			mapCommands.put("ar condicionado desligar", new UtensilioDesligarCommand(new ArCondicionado()));
-			mapCommands.put("televisor ligar", new UtensilioLigarCommand(new Televisor()));
-			mapCommands.put("televisor desligar", new UtensilioDesligarCommand(new Televisor()));
-			mapCommands.put("portão abrir", new UtensilioLigarCommand(new Televisor()));
-			mapCommands.put("portão fechar", new UtensilioDesligarCommand(new Televisor()));
 		}
-
 		return instance;
+	}
+	
+	public SingletonCommands() {
+		this.mapCommands = new HashMap<String, Command>();
+		mapCommands.put("lâmpada ligar", new UtensilioLigarCommand(new Lampada()));
+		mapCommands.put("lâmpada desligar", new UtensilioDesligarCommand(new Lampada()));
+		mapCommands.put("ar condicionado ligar", new UtensilioLigarCommand(new ArCondicionado()));
+		mapCommands.put("ar condicionado desligar", new UtensilioDesligarCommand(new ArCondicionado()));
+		mapCommands.put("televisor ligar", new UtensilioLigarCommand(new Televisor()));
+		mapCommands.put("televisor desligar", new UtensilioDesligarCommand(new Televisor()));
+		mapCommands.put("portão abrir", new UtensilioLigarCommand(new Portao()));
+		mapCommands.put("portão fechar", new UtensilioDesligarCommand(new Portao()));
 	}
 
 	public Map<String, Command> getMapCommands() {
